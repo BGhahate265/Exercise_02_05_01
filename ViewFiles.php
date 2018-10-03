2a87ee86a194dbe7e0886d71d9d716b1dd722137
Unknown <bghahate265@west-mec.org>
2018-10-02 18:26:02 -0700

    Second Commit

---
+++ ViewFiles.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="initial-scale=1.0">
+        <script src="modernizr.custom.65897.js"></script>
+    </head>
+    <body>
+        <h2>View Files</h2>
+        <?php
+        $dir = "../Exercise_02_04_01";
+        $openDir = opendir($dir);
+        while($curFile = readdir($openDir)) {
+            if (strcmp($curFile, '.') !== 0 && strcmp($curFile, '..') !== 0) {
+                echo "<a href=\"$dir/$curFile\">$curFile</a><br>\n";
+            }
+        }
+        closedir($openDir);
+        ?>
+        
+    </body>
+</html>
